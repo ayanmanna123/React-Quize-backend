4168f51e50da8d86647fec4d1b3284a31d52d86c
Ayan Manna <ayanmanna858@gmail.com>
2025-04-09 23:41:57 +0530

    new

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/', {
+        await mongoose.connect('mongodb+srv://mannaayan777:ayanmanna7777777@cluster0.8uslw.mongodb.net/singingagain?retryWrites=true&w=majority&appName=Cluster0', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
